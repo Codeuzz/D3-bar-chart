d5baef5fe9c59ff511d68d896be08a7c16c72741
Codeuzz <belinda.zilevu@gmail.com>
2024-09-28 13:13:36 +0200

    need to scale rect data to SVG and add axisLeft

--- main.js
+++ main.js
@@ -2,7 +2,8 @@ import './style.css'
 import * as d3 from 'd3';
 
 document.querySelector('#app').innerHTML = `
-  <h1>D3 Bar Chart</h1>
+  <h1>United States GDP</h1>
+  <div id='tooltip'></div>
 `
 
 fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/master/GDP-data.json')
@@ -19,29 +20,54 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
 
 const barMan = (data) => {
  const donnee = data.data.map(d => d)
-
  const svg = d3.select('svg')
 
+ const getQuarter = (dateString) => {
+  const month = parseInt(dateString.split('-')[1], 10); 
+  const year = parseInt(dateString.split('-')[0], 10)
+  if (month >= 1 && month <= 3) return year + ' Q1';
+  if (month >= 4 && month <= 6) return year + ' Q2';
+  if (month >= 7 && month <= 9) return year + ' Q3';
+  return year + ' Q4';
+};
+
+ const tooltip = d3.select('#tooltip')
+
  svg.selectAll('rect')
- .data(donnee.map(d => d[1]))
+ .data(donnee.map(d => d))
  .enter()
  .append('rect')
- .style('height', d => `${d / 40}px`)
- .style('width', 3)
+ .style('height', d => `${d[1] / 40}px`)
+ .style('width', 4)
  .attr('class', 'bar')
- .attr('x', (d, i) => i * 3 )
- .attr("y", d => height - d / 40 - padding)
+ .attr('x', (d, i) => i * 4.1 )
+ .attr("y", d => height - d[1] / 40 - padding)
  .attr('fill', 'aqua')
+ .on('mouseover', (event, d) => {
+  tooltip.style('visibility', 'visible')
+         .html(`${getQuarter(d[0])} <br> $${d[1].toLocaleString()} Billion`)
+         .style('top', `${height - padding}px`)
+         .style('left', `${event.pageX + 15}px`);
+  })
+.on('mouseout', () => {
+  tooltip.style('visibility', 'hidden'); 
+  });
+ 
+
+//  .append('title')
+//  .text(d => `
+//   ${getQuarter(d[0])} 
+//   $${d[1].toLocaleString()} Billion
+//   `)
  
 }
 
 
 const height = 550;
 const width = 1100;
-const padding = 30;
-const x = -40;
+const padding = 20;
 
-const svg =  d3.select('body')
+const svg =  d3.select('div')
     .append('svg')
     .attr('height', height)
     .attr('width', width)
@@ -49,7 +75,7 @@ const svg =  d3.select('body')
 
     const scale = d3.scaleLinear()
     .domain([1947, 2015])  
-    .range([0, 1087]);
+    .range([0, 1100]);
 
     const axis = d3.axisBottom(scale);
 
