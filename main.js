813d9dbbf997a96c7da85afa4967a2d87e1d585e
Codeuzz <belinda.zilevu@gmail.com>
2024-09-30 09:23:22 +0200

    finished x scaling need to do y

--- main.js
+++ main.js
@@ -16,8 +16,11 @@ fetch('https://raw.githubusercontent.com/freeCodeCamp/ProjectReferenceData/maste
 })
 .then(data => {
   makeSvg(data)
-  barMan(data)
   makeAxis(data)
+  if(scale && scale2) {
+   barMan(data)
+
+  }
 
 })
 
@@ -33,40 +36,41 @@ const makeSvg = (data) => {
     .append('svg')
     .attr('height', height)
     .attr('width', width)
-    .style('border', '1px solid white')
-
+    // .style('border', '1px solid white')
+    
 }
 
-//        NEED TO DO GETYEAR FUNCTION TO ADD IT TO BOTTOM SCALE DOMAIN AND REUSE IT IN GETQUARTER ??
 
-// const getYear = (dateString) => {
-//   const year = parseInt(dateString.split('-')[0], 10)
-//   return year
-// }
+const getYear = (dateString) => {
+  const year = parseInt(dateString.split('-')[0], 10)
+  return year
+}
 
+let scale, scale2;
 const makeAxis = (data) => {
   const donnee = data.data.map(d => d)
   const svg = d3.select('svg')
 
   
 
-  const scale = d3.scaleLinear()
+   scale = d3.scaleLinear()
   .domain([1947, 2016])  
-  .range([padding, width - padding]);
+  .range([padding, width]);
 
   const axisX = d3.axisBottom(scale);
   svg.append('g')
   .attr("transform", `translate(0, ${height - padding})`)
   .call(axisX);
 
-  const scale2 = d3.scaleLinear()
+  scale2 = d3.scaleLinear()
   .domain([0, d3.max(donnee.map(d => d[1]))])
   .range([height - padding, padding])
-  // console.log(d3.max(donnee.map(d => d[1])))
+
   const axisY = d3.axisLeft(scale2)
   svg.append('g')
       .attr("transform", `translate(${padding}, 0)`)
       .call(axisY);
+
 }
 
 
@@ -77,28 +81,33 @@ const barMan = (data) => {
 
  const getQuarter = (dateString) => {
   const month = parseInt(dateString.split('-')[1], 10); 
-  const year = parseInt(dateString.split('-')[0], 10)
-  if (month >= 1 && month <= 3) return year + ' Q1';
-  if (month >= 4 && month <= 6) return year + ' Q2';
-  if (month >= 7 && month <= 9) return year + ' Q3';
-  return year + ' Q4';
+  const year = getYear(dateString)
+  if (month >= 1 && month <= 3) return { year: year, quarter: 'Q1', offset: 0};
+  if (month >= 4 && month <= 6) return { year: year, quarter: 'Q2', offset: 0.25 };
+  if (month >= 7 && month <= 9) return { year: year, quarter: 'Q3', offset: 0.5 };
+  return { year: year, quarter: 'Q4', offset: 0.75 }
 };
 
  const tooltip = d3.select('#tooltip')
 
  svg.selectAll('rect')
- .data(donnee.map(d => d))
+ .data(donnee)
  .enter()
  .append('rect')
  .style('height', d => `${d[1] / 40}px`)
  .style('width', 3)
  .attr('class', 'bar')
- .attr('x', (d, i) => i * 3.1 + padding )
+ .attr('x', (d, i) => {
+    const {year, offset} = getQuarter(d[0])
+    
+    return scale(year) + (offset * (scale(1948) - scale(1947)))
+ })
  .attr("y", d => height - d[1] / 40 - padding)
  .attr('fill', 'aqua')
  .on('mouseover', (event, d) => {
+  const { quarter, year } = getQuarter(d[0]);
   tooltip.style('visibility', 'visible')
-         .html(`${getQuarter(d[0])} <br> $${d[1].toLocaleString('us-US')} Billion`)
+         .html(`${year} ${quarter} <br> $${d[1].toLocaleString('us-US')} Billion`)
          .style('top', `${height - padding - 50}px`)
          .style('left', `${event.pageX + padding}px`);
   })
@@ -107,47 +116,8 @@ const barMan = (data) => {
   });
  
 
-//  .append('title')
-//  .text(d => `
-//   ${getQuarter(d[0])} 
-//   $${d[1].toLocaleString()} Billion
-//   `)
  
 }
-
-
-
-
-// const svg =  d3.select('div')
-//     .append('svg')
-//     .attr('height', height)
-//     .attr('width', width)
-//     .style('border', '1px solid white')
-
-//     const scale = d3.scaleLinear()
-//     .domain([1947, 2015])  
-//     .range([0, 1100]);
-
-//     console.log(scale())
-
-//     const axis = d3.axisBottom(scale);
-
-//     svg.append('g')
-//     .attr("transform", `translate(0, ${height - padding})`)
-//     .call(axis);
-
-    
-
-
-
-
-
-
-// .append('rect')
-// .attr('x', 0)
-// .attr('y', 0)
-// .attr('height', 10)
-// .attr('width', 10)
 
 
 
